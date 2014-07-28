b1a21d63e3f591df8ddd604c5aeb3b470062e1bd
Ned Ruggeri <ruggeri@self-loop.com>
2014-07-28 11:12:06 -0700

    check in SKELETON

---
+++ skeleton/lib/board.js
@@ -0,0 +1,13 @@
+var Piece = require("./piece.js").Piece;
+
+function Board() {
+  // this.grid =
+}
+
+Board.prototype.full = function () {
+
+};
+
+// Other helper methods may be helpful!
+
+exports.Board = Board;
---
+++ skeleton/lib/piece.js
@@ -0,0 +1,9 @@
+function Piece(color) {
+  this.color = color;
+}
+
+Piece.prototype.flip = function () {
+  // fill me in
+};
+
+exports.Piece = Piece;
