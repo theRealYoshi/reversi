37d1cbbb224dd888a470d83b96ba22056527ad44
Will Hastings <will@objectdotcreate.net>
2014-09-29 17:13:27 -0700

    Add method descriptions to Board. (#3)

--- solution/lib/board.js
+++ solution/lib/board.js
@@ -1,5 +1,9 @@
 var Piece = require("./piece");
 
+/**
+ * Returns a 2D array (8 by 8) with two black pieces at [3, 4] and [4, 3]
+ * and two white pieces at [3, 3] and [4, 4]
+ */
 function _makeGrid () {
   var grid = [];
 
@@ -17,6 +21,9 @@ function _makeGrid () {
   return grid;
 }
 
+/**
+ * Constructs a Board with a starting grid set up.
+ */
 function Board () {
   this.grid = _makeGrid();
 }
@@ -27,6 +34,10 @@ Board.DIRS = [
   [-1,  0], [-1,  1]
 ];
 
+/**
+ * Returns the piece at a given [x, y] position,
+ * throwing an Error if the position is invalid.
+ */
 Board.prototype.getPiece = function (pos) {
   if (!this.isValidPos(pos)) {
     throw new Error("Not valid pos!");
@@ -35,10 +46,16 @@ Board.prototype.getPiece = function (pos) {
   return this.grid[pos[0]][pos[1]];
 };
 
+/**
+ * Checks if there are any valid moves for the given color.
+ */
 Board.prototype.hasMove = function (color) {
   return this.validMoves(color).length !== 0;
 };
 
+/**
+ * Checks if every position on the Board is occupied.
+ */
 Board.prototype.isFull = function () {
   for (var i = 0; i < 8; i++) {
     for (var j = 0; j < 8; j++) {
@@ -50,23 +67,50 @@ Board.prototype.isFull = function () {
   return true;
 };
 
+/**
+ * Checks if the piece at a given position
+ * matches a given color.
+ */
 Board.prototype.isMine = function (pos, color) {
   var piece = this.getPiece(pos);
   return piece && piece.color === color;
 };
 
+/**
+ * Checks if a given position has a piece on it.
+ */
 Board.prototype.isOccupied = function (pos) {
   return !!this.getPiece(pos);
 };
 
+/**
+ * Checks if both the white player and
+ * the black player are out of moves.
+ */
 Board.prototype.isOver = function () {
   return !this.hasMove("black") && !this.hasMove("white");
 };
 
+/**
+ * Checks if a given position is on the Board.
+ */
 Board.prototype.isValidPos = function (pos) {
   return (pos[0] >= 0 && pos[0] < 8) && (pos[1] >= 0 && pos[1] < 8);
 };
 
+/**
+ * Recursively follows a direction away from a starting position, adding each
+ * piece of the opposite color until hitting another piece of the current color.
+ * It then returns an array of all pieces between the starting position and
+ * ending position.
+ *
+ * Returns null if it reaches the end of the board before finding another piece
+ * of the same color.
+ *
+ * Returns null if it hits an empty position.
+ *
+ * Returns null if no pieces of the opposite color are found.
+ */
 function _positionsToFlip (board, pos, color, dir, piecesToFlip) {
   if (!piecesToFlip) {
     piecesToFlip = [];
@@ -87,6 +131,12 @@ function _positionsToFlip (board, pos, color, dir, piecesToFlip) {
   }
 }
 
+/**
+ * Adds a new piece of the given color to the given position, flipping the
+ * color of any pieces that are eligible for flipping.
+ *
+ * Throws an error if the position represents an invalid move.
+ */
 Board.prototype.placePiece = function (pos, color) {
   if (!this.validMove(pos, color)) {
     throw new Error('Invalid move!');
@@ -107,6 +157,9 @@ Board.prototype.placePiece = function (pos, color) {
   this.grid[pos[0]][pos[1]] = new Piece(color);
 };
 
+/**
+ * Prints a string representation of the Board to the console.
+ */
 Board.prototype.print = function () {
   for (var i = 0; i < 8; i++) {
     var rowString = " " + i + " |";
@@ -121,6 +174,11 @@ Board.prototype.print = function () {
   }
 };
 
+/**
+ * Checks that a position is not already occupied and that the color
+ * taking the position will result in some pieces of the opposite
+ * color being flipped.
+ */
 Board.prototype.validMove = function (pos, color) {
   if (this.isOccupied(pos)) {
     return false;
@@ -137,6 +195,10 @@ Board.prototype.validMove = function (pos, color) {
   return false;
 };
 
+/**
+ * Produces an array of all valid positions on
+ * the Board for a given color.
+ */
 Board.prototype.validMoves = function (color) {
   var validMovesList = [];
 
