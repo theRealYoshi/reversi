3b13cb7533f855d892066b036e3719fdddf98cce
Will Hastings <will@objectdotcreate.net>
2014-10-01 13:57:36 -0700

    Copy rewritten tests to skeleton. (#1)

--- skeleton/test/test.js
+++ skeleton/test/test.js
@@ -4,8 +4,13 @@ var Piece = require("../lib/piece.js");
 var Game = require("../lib/game.js");
 
 describe("Piece", function () {
-  var darkPiece = new Piece("black");
-  var lightPiece = new Piece("white");
+  var darkPiece,
+      lightPiece;
+
+  beforeEach(function () {
+    darkPiece = new Piece("black");
+    lightPiece = new Piece("white");
+  });
 
   describe("#color", function () {
     it("should have the color assigned to it", function () {
@@ -13,22 +18,42 @@ describe("Piece", function () {
       assert.equal(lightPiece.color, "white");
     });
   });
-
   describe("#flip", function () {
     it("should switch colors when flipped", function () {
       darkPiece.flip();
       assert.equal(darkPiece.color, "white");
+      lightPiece.flip();
+      assert.equal(lightPiece.color, "black");
     });
   });
-})
+
+  describe('#toString', function () {
+    it('should return a letter representing the color', function() {
+      assert.equal(lightPiece.toString(), 'W');
+      assert.equal(darkPiece.toString(), 'B');
+    });
+  });
+});
 
 describe("Board", function () {
-  var testBoard = new Board();
+  var testBoard;
+
+  beforeEach(function () {
+    testBoard = new Board();
+  });
+
+  var fillBoard = function(board) {
+    for (var i = 0; i < 8; i ++) {
+      for (var j = 0; j < 8; j ++) {
+        board.grid[i][j] = new Piece("white");
+      }
+    }
+  };
 
   describe("#grid", function () {
     it("should be 8x8", function () {
       assert.equal(testBoard.grid.length, 8);
-      for(i = 0; i < 8; i ++){
+      for(var i = 0; i < 8; i ++){
         assert.equal(testBoard.grid[i].length, 8);
       }
     });
@@ -48,49 +73,106 @@ describe("Board", function () {
           }
         });
       });
-    })
+    });
   });
 
-  describe("putting pieces on board", function(){
-    // TODO.
+  describe('#getPiece', function () {
+    it('should return a piece for an occupied position', function () {
+      assert(testBoard.getPiece([3, 4]) instanceof Piece, true);
+    });
+
+    it('should return undefined for an empty position', function() {
+      assert.equal(testBoard.getPiece([0, 0]), undefined);
+    });
+
+    it('should throw an error for an invalid position', function() {
+      function pickInvalidPos() {
+        testBoard.getPiece([10, 10]);
+      }
+      assert.throws(pickInvalidPos, Error, 'Not valid pos!');
+    });
   });
 
-  describe("#full", function () {
+  describe('#hasMove', function () {
+    it('should return true when a color has one or more moves', function() {
+      assert.equal(testBoard.hasMove('black'), true);
+    });
+
+    it('should return false when a color has no more moves', function() {
+      fillBoard(testBoard);
+      assert.equal(testBoard.hasMove('black'), false);
+    });
+  });
+
+  describe("#isFull", function () {
     it("should not begin full", function () {
       var anotherBoard = new Board();
-      assert.equal(anotherBoard.full(), false);
+      assert.equal(anotherBoard.isFull(), false);
     });
 
     it("should be full if no spots are empty", function () {
       // Fill the board for the test
-      for (i = 0; i < 8; i ++) {
-        for (j = 0; j < 8; j ++) {
-          testBoard.grid[i][j] = new Piece("white");
-        }
-      }
-
+      fillBoard(testBoard);
       // Test fullness
-      assert.equal(testBoard.full(), true);
+      assert.equal(testBoard.isFull(), true);
     });
   });
-});
 
-describe("Game", function () {
+  describe('#isMine', function() {
+    it('should return true when the retrieved piece matches the color', function() {
+      assert.equal(testBoard.isMine([3, 3], 'white'), true);
+    });
+
+    it('should return false when retrieved piece does not match', function() {
+      assert.equal(testBoard.isMine([3, 3], 'black'), false);
+    });
+
+    it('should return falsey when retrieved piece is undefined', function() {
+      assert.equal(!testBoard.isMine([0, 0], 'black'), true);
+    });
+  });
+
+  describe('#isOver', function () {
+    it('should return false at the start', function() {
+      assert.equal(testBoard.isOver(), false);
+    });
+
+    it('should return true when there are no more moves', function() {
+      fillBoard(testBoard);
+      assert.equal(testBoard.isOver(), true);
+    });
+  });
+
+  describe('#isValidPos', function () {
+    it('should return false when x or y is less than 0', function() {
+      assert.equal(testBoard.isValidPos([-1, 1]), false);
+      assert.equal(testBoard.isValidPos([1, -1]), false);
+    });
+
+    it('should return false when x or y is greater than 7', function() {
+      assert.equal(testBoard.isValidPos([9, 1]), false);
+      assert.equal(testBoard.isValidPos([1, 9]), false);
+    });
+
+    it('should return true otherwise', function() {
+      assert.equal(testBoard.isValidPos([3, 3]), true);
+    });
+  });
+
   describe("#placePiece", function () {
-    var testGame = new Game();
-
     it("should allow a player to make a valid move", function(){
-      testGame.placePiece([3, 2], "black");
-      assert.equal(testGame.board.grid[3][2].color, "black");
+      testBoard.placePiece([2, 3], "black");
+      assert.equal(testBoard.grid[2][3].color, "black");
     });
 
     it("should flip captured pieces", function(){
-      assert.equal(testGame.board.grid[3][3].color, "black");
+      testBoard.placePiece([2, 3], "black");
+      assert.equal(testBoard.grid[3][3].color, "black");
     });
 
     it("should not allow a piece on top of another piece", function () {
       function makeBadMove () {
-        testGame.placePiece([3, 2], "white");
+        testBoard.placePiece([3, 2], "white");
       }
 
       assert.throws(makeBadMove, Error, "Invalid Move");
@@ -98,7 +180,7 @@ describe("Game", function () {
 
     it("should not allow a move that doesn't capture", function () {
       function makeBadMove () {
-        testGame.placePiece([2, 3], "white");
+        testBoard.placePiece([2, 3], "white");
       }
 
       assert.throws(makeBadMove, Error, "Invalid Move");
@@ -106,23 +188,49 @@ describe("Game", function () {
 
     it("should not allow moves that isolate pieces", function () {
       function makeOtherBadMove () {
-        testGame.placePiece([0, 0], "white");
+        testBoard.placePiece([0, 0], "white");
       }
 
       assert.throws(makeOtherBadMove, Error, "Invalid Move");
     });
   });
 
-  describe("players taking turns", function () {
-    var anotherGame = new Game();
-
-    it("should require black to take first turn", function () {
-      assert.equal(anotherGame.currentPlayer, "black");
+  describe('#validMove', function () {
+    it('should return false for an occupied position', function() {
+      assert.equal(testBoard.validMove([3, 4], 'black'), false);
     });
 
-    it("should switch players after one takes a turn", function () {
-      anotherGame.placePiece([[2, 3], "black"]);
-      assert.equal(anotherGame.currentPlayer, "white");
+    it('should return false for a move that does not capture', function() {
+      assert.equal(testBoard.validMove([2, 3], "white"), false);
+    });
+
+    it('should return true for a valid move', function() {
+      assert.equal(testBoard.validMove([2, 3], "black"), true);
+    });
+  });
+
+  describe('#validMoves', function () {
+    it('should return an empty array when there are no valid moves', function() {
+      fillBoard(testBoard);
+      assert.equal(testBoard.validMoves('black').length, 0);
+    });
+
+    it('should return an array of valid moves when some are left', function() {
+      var validPositions = [
+        [2, 3],
+        [3, 2],
+        [4, 5],
+        [5, 4]
+      ];
+      var actualPositions = testBoard.validMoves('black');
+
+      assert.equal(actualPositions.length, validPositions.length);
+
+      actualPositions.forEach(function(actualPosition, index) {
+        var validPosition = validPositions[index];
+        assert.equal(actualPosition[0], validPosition[0]);
+        assert.equal(actualPosition[1], validPosition[1]);
+      });
     });
   });
 });
